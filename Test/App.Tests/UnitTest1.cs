761c8911e111adf0062fbc0633eb5c5a070301f7
DevTee4 <toyinayodele2012@gmail.com>
2021-08-15 20:13:15 +0100

    Unit test fixing

--- Test/App.Tests/UnitTest1.cs
+++ Test/App.Tests/UnitTest1.cs
@@ -3,57 +3,64 @@ using InMemoryWebAPI.Controllers;
 using InMemoryWebAPI.Models;
 using System.Collections.Generic;
 using Microsoft.Extensions.Caching.Memory;
+using System;
+
+using Microsoft.AspNetCore.Mvc;
 
 namespace App.Tests
 {
+    using Xunit;
     [TestClass]
     public class UnitTest1
     {
 
         UserController controller;
         private readonly IMemoryCache _memoryCache = new MemoryCache(new MemoryCacheOptions());
-        List<User> users = new List<User>(){
-            new User(){ Fullname="Angel Gomez", Username= "gomezz",Email="gomeza@hotmail.com",Password="craft"},
-            new User(){ Fullname="Makinde Joy", Username= "joyee",Email="joyee@hotmail.com",Password="craft"},
-            new User(){ Fullname="Dancer Maker", Username= "makerD",Email="makerd@hotmail.com",Password="craft"}
-        };
-
         public UnitTest1(){
            controller = new UserController(_memoryCache);
         }
         
-        [TestMethod]
+        [Fact]
         public void TestAddUser()
         {
-            // ActionResult<Post> result= controller.Create(new User{
-            //     Fullname="Angel Gomez", Username= "gomezz",Email="gomeza@hotmail.com",Password="craft"
-            // });
-            // Post createdPost = result.Value;
-
+            var  result= controller.Create(new User{
+                Fullname="Angel Gomez", Username= "gomezz",Email="gomeza@hotmail.com",Password="craft"
+            });
+            Assert.NotNull(result);
+            Assert.IsType<User>(result.Value);     
         }
         [TestMethod]
         public void TestGet()
         {
-            
+          var  result= controller.GetUser("gomeza@hotmail.com","craft");
+          Assert.NotNull(result);
 
         }
-        [TestMethod]
-        public void TestGetMyBalance()
-        {
-            
-
-        }
-        [TestMethod]
+         [Fact]
         public void TestFundMyAccount()
         {
-            
+          var  result= controller.FundAccount(new UserUpdate() {
+          Email="gomeza@hotmail.com",
+          Amount=800.0
+          });
+         Xunit.Assert.Equal("Account successfully funded, your balance is : $800",result.Value);
+        }
+        [Fact]
+        public void TestGetMyBalance()
+        {
+             var  result= controller.GetMyBalance("gomeza@hotmail.com");
+             Xunit.Assert.Equal("$500",result.Value.balance);
 
         }
-        [TestMethod]
+       
+        [Fact]
         public void TestSendMoney()
         {
-            
-
+           var  result= controller.Create(new User{
+                Fullname="Dancer Whales", Username= "dancee",Email="dancee@hotmail.com",Password="craft"
+          });
+          var  result= controller.SendMoney("gomeza@hotmail.com","dancee@hotmail.com",500.0);
+          Xunit.Assert.Equal("Money successfully sent, your balance is : $300",result.Value.message);
         }
     }
 }
